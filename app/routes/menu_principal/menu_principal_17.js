58edfcfba4ebde21cae56dbff09284c256ce89d2
Clodoaldo Dias <clodoaldo.dias@grupo.altamira.com.br>
2018-03-15 09:55:37 -0300

    -Updade2

--- app/routes/menu_principal/menu_principal_17.js
+++ app/routes/menu_principal/menu_principal_17.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
     app.get('/menu_principal_17', function(req,res){
         res.render('menu_principal/menu_principal_17');
-        req.assert('etam_A','Não pode ser vazio').notEmpty();
+        
     });
     
 
